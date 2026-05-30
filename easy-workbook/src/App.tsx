820b648515014170d582430b9489bdacbbb4b4a8
gvenkatesh-12 <gvenkatesh.on@gmail.com>
2026-05-30 08:54:55 +0530

    feat: implement dynamic dark mode toggling with CSS variable updates

--- easy-workbook/src/App.tsx
+++ easy-workbook/src/App.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
 import { AppShell } from '@/components/layout/AppShell';
+import { useUiStore } from '@/store/uiStore';
 
 function App() {
+  const darkMode = useUiStore((s) => s.darkMode);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
+    }
+  }, [darkMode]);
+
   return <AppShell />;
 }
 
